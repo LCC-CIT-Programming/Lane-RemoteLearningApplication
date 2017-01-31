fd86cffa1e0d470e0bd44d622d44c123a641ca4a
DevinDuBose <devin.a.dubose@gmail.com>
2017-01-31 15:36:38 -0800

    Unit testing
    App User and Student Classes have working tests, Student now extends
    AppUser.

---
+++ Models/AppUser.php
@@ -0,0 +1,65 @@
+
+<?php
+
+class AppUser {
+    protected $userID, $firstName, $lastName, $lnumber, $pass, $email;
+
+    public function __construct($userID, $firstName, $lastName, $lNumber, $pass, $email) {
+        $this->userID = $userID;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+		    $this->lnumber = $lNumber;
+		    $this->pass = $pass;
+		    $this->email = $email;
+    }
+
+    public function getUserID() {
+        return $this->userID;
+    }
+
+    // public function setUserID($value) {
+    //     $this->userID = $value;
+    // }
+
+    public function getFirstName() {
+        return $this->firstName;
+    }
+
+    public function setFirstName($value) {
+        $this->firstName = $value;
+    }
+
+	public function getLastName() {
+        return $this->lastName;
+    }
+
+    public function setLastName($value) {
+        $this->lastName = $value;
+    }
+
+	public function getLNumber() {
+		return $this->lnumber;
+	}
+
+  // public function setLNumber($value) {
+  //       $this->lnumber = $value;
+  // }
+
+	public function getPassword() {
+		return $this->pass;
+	}
+
+	public function setPassword($value) {
+		   $this->pass = $value;
+	}
+
+	public function getEmail() {
+		return $this->email;
+	}
+
+	public function setEmail($value) {
+		   $this->email = $value;
+	}
+
+}
+?>
