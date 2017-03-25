30507aa1e55e448929f081d9fff6015a559d6dbf
Devin <devin.a.dubose@gmail.com>
2017-03-25 13:27:05 -0700

    Correct index changes

--- Index.php
+++ Index.php
@@ -1,21 +1,28 @@
 <?php
-//last working 2-24
-require_once('./Models/appUser.php');
-require_once('./Models/student.php');
-require_once('./Models/studentDB.php');
-require_once('./Models/db.php');
-require_once('./Models/course.php');
-require_once('./Models/courseDB.php');
-require_once('./Models/question.php');
-require_once('./Models/questionDB.php');
-require_once('./Models/tutor.php');
-require_once('./Models/tutorDB.php');
-require_once('./Models/visit.php');
-require_once('./Models/visitdb.php');
-
-session_start();
-
+require_once('/Models/appuser.php');
+require_once('/Models/student.php');
+require_once('/Models/studentdb.php');
+require_once('/Models/db.php');
+require_once('/Models/course.php');
+require_once('/Models/coursedb.php');
+require_once('/Models/question.php');
+require_once('/Models/questiondb.php');
+require_once('/Models/tutor.php');
+require_once('/Models/tutordb.php');
+require_once('/Models/visit.php');
+require_once('/Models/visitdb.php');
+require_once('/Models/task.php');
+require_once('/Models/taskdb.php');
 try {
+session_start();
+if (isset($_SESSION['user']))
+		$user = $_SESSION['user'];
+if (isset($_SESSION['courses']))
+		$courses = $_SESSION['courses'];
+if (isset($_SESSION['visit']))
+		$visit = $_SESSION['visit'];
+if (isset($_SESSION['task']))
+		$task = $_SESSION['task'];
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL){
 	$action = filter_input(INPUT_GET, 'action');
@@ -23,97 +30,99 @@ if ($action == NULL){
 		$action = 'default';
 	}
 }
-
-
 switch($action) {
 	case "default":
 			$_SESSION['user'] = null;
 			$loginError = "";
 			include("./Views/login.php");
 	break;
-
 	case "login":
-			
 			$username = filter_input(INPUT_POST, "lnumber");
 			$password = filter_input(INPUT_POST, "password");
 			$role = filter_input(INPUT_POST, "roleSelect");
-
 			if ($role == "isStudent") {
 					$user = StudentDB::StudentLogin($username, $password);
-
 					if ($user !== null && isset($user)) {
 						  $courses = StudentDB::GetStudentCourses($user);
 							$_SESSION['user'] = $user;
 							$_SESSION['courses'] = $courses;
 							$visit = new Visit($user->GetUserID(), 1, date("Y-m-d h:i:s"));
 							VisitDB::CreateVisit($visit);
-
-							include("./Views/home.php");
+							$visit = VisitDB::RetrieveVisit($visit);
+							$_SESSION['visit'] = $visit;
+							$task = new Task($visit->getVisitID(), $courses[0]->getCourseNumber(),date("Y-m-d h:i:s"));
+							TaskDB::CreateTask($task);
+							$task = TaskDB::RetrieveTask($task);
+							$_SESSION['task'] = $task;
+							include("/Views/home.php");
 					} else {
 							$_SESSION['user'] = null;
 							$loginError = "Login attempt failed.";
 							include("./Views/login.php");
 					}
+				}
+				else{
+						$user = TutorDB::TutorLogin($username, $password);
+						if ($user !== null && isset($user)) {
+								$userID = $user->GetUserID();
+								$startTime = date("Y-m-d h:i:s");
+								$locationID = 1;
+								$visit = new Visit($userID, $locationID, $startTime);
+								$_SESSION['visit'] = $visit;
+								$_SESSION['user'] = $user;
+								VisitDB::CreateVisit($visit);
+					}
 			}
-			else {
-				$user = TutorDB::TutorLogin($username, $password);
-
-				if ($user !== null && isset($user)) {
-						$userID = $user->GetUserID();
-						$startTime = date("Y-m-d h:i:s");
-						$locationID = 1;
-						$visit = new Visit($userID, $locationID, $startTime);
-						$_SESSION['visit'] = $visit;
-						$_SESSION['user'] = $user;
-						VisitDB::CreateVisit($visit);
-			}
-		}
 	break;
 	case "ask":
 		$questionError = "";
 		$success = "";
 		include("./Views/ask.php");
 	break;
-	
 	case "ask_question":
-
 		$courseNum = filter_input(INPUT_POST, "courseSelect");
-		$subject = filter_input(INPUT_POST, "subject");	
+		$subject = filter_input(INPUT_POST, "subject");
 		$description = filter_input(INPUT_POST, "description");
 		$status = "open";
 		$askTime = date("Y-m-d h:i:s");
-		
-		if ($courseNum == null || $subject == null || 
+		if ($courseNum == null || $subject == null ||
 			$description == null || $status == null || $askTime == null){
 				$questionError = "Invalid question. Check all fields and try again.";
-		
-				include("./Views/ask.php");	
-			}
+				$success = "";
+				include("./Views/ask.php");
+		}
 		else{
 			$user = $_SESSION['user'];
 			$userID = $user->GetUserID();
 			$question = new Question($userID, $courseNum, $subject, $description, $status, $askTime);
 			QuestionDB::CreateQuestion($question);
-			
-			include("./Views/home.php");	
+			$success = "Question created, ask another?";
+			$questionError = "";
+			$task = $_SESSION['task'];
+			$task->setEndTime(date("Y-m-d h:i:s"));
+			taskdb::UpdateTask($task);
+			$startNewTask = new Task($visit->getVisitID(), $courseNum ,date("Y-m-d h:i:s"));
+			taskdb::CreateTask($startNewTask);
+			$task = taskdb::RetrieveTask($startNewTask);
+			$_SESSION['task'] = $task;
+			include("./Views/ask.php");
 		}
 	break;
-	
 	case "logout":
 			$_SESSION['user'] = null;
 			$loginError = "";
 			session_destroy();
 			include("./Views/login.php");
 	break;
-	
 	case "home":
 		include("./Views/home.php");
 	break;
-	
 	case "schedule":
 		include("./Views/schedule.php");
 	break;
-	
+	case "ask":
+			include("/Views/ask.php");
+	break;
 	case "edit":
 		$success = "";
 		$passError = "";
@@ -126,7 +135,6 @@ switch($action) {
 		$email = filter_input(INPUT_POST, "email");
 		$pass1 = filter_input(INPUT_POST, "newPwd1");
 		$pass2 = filter_input(INPUT_POST, "newPwd2");
-
 		if ($pass1 != $pass2){	
 			$passError = "Sorry the passwords do not match, please try again.";
 			$success = "";
@@ -151,7 +159,6 @@ switch($action) {
 		$pass1 = filter_input(INPUT_POST, "newPwd1");
 		$pass2 = filter_input(INPUT_POST, "newPwd2");
 		$summary = filter_input(INPUT_POST, "summary");
-
 		if ($pass1 != $pass2){	
 			$passError = "Sorry the passwords do not match, please try again.";
 			$success = "";
@@ -179,6 +186,4 @@ switch($action) {
 		include('./Errors/database_error.php');
 		exit();
 }
-
-
  ?>
