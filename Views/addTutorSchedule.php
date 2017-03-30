c6dc717011681419794d614e40b053e68312ce5c
Devin <devin.a.dubose@gmail.com>
2017-03-30 12:18:37 -0700

    Add more shift inputs to schedule

--- Views/addTutorSchedule.php
+++ Views/addTutorSchedule.php
@@ -11,7 +11,7 @@
 	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	 
+
 	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
    </head>
@@ -20,33 +20,35 @@
 <body>
 <div class="title">
 	  <div class="container text-center">
-		<h1>Lane Community College CIT Lab Edit Profile</h1>
+		<h1>Lane Community College CIT Lab Edit Schedule</h1>
 	  </div>
 	</div>
 	<div class="container-fluid" style="width: 500px">
-	
+
 	<form class="">
-	<div class = "form-group">
-	<button class="btn btn-success pull-right" style="border-radius: 50%;"><i class="fa fa-plus" aria-hidden="true"></i></button>
-	
-		<div class = "row">
-			<div class = "col-md-4">
-				<input type="date" style="width:100%" />
-			</div>
-			
-			<div class = "col-md-3">
-				<input type="time" style="width:100%"/>
-			</div>
-			<div class = "col-md-3">
-				<input type="time" style="width:100%"/>
-			</div>
+  	<div class = "form-group">
+  	<button class="btn btn-success pull-right add_field" style="border-radius: 50%;"><i class="fa fa-plus" aria-hidden="true"></i></button>
+
+      <div class="shifts-container">
+    		<div class = "row">
+    			<div class = "col-md-4">
+    				<input type="date" name="shifts[]" style="width:100%" />
+    			</div>
+
+    			<div class = "col-md-3">
+    				<input type="time" name="shifts[]" style="width:100%"/>
+    			</div>
+    			<div class = "col-md-3">
+    				<input type="time" name="shifts[]" style="width:100%"/>
+    			</div>
+    		</div>
+      </div>
 		</div>
-		</div>
-		
+
 		<button type="submit" class="btn btn-primary">Add</button>
 		</form>
-		
-		
+
+
 		<table class="table table-responsive table-condensed">
 		<thead>
 			<tr>
@@ -59,10 +61,10 @@
 			</tr>
 		</tbody>
 		</table>
-				
+
 	</div>
 
-	
+
 </body>
 <footer class="col-lg-12 container-fluid text-center" style="position: fixed; bottom: 0; width: 100%;">
 		<div class="container">
@@ -70,3 +72,27 @@
 		</div>
 </footer>
 </html>
+
+<script>
+$(document).ready(function() {
+    var max_fields = 5; //maximum input boxes allowed
+    var wrapper    = $(".shifts-container"); //Fields wrapper
+    var add_button = $(".add_field"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            //$(wrapper).append('<div><input type="text" name="shifts[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+            $(wrapper).append('<div class = "row"> <div class = "col-md-4"><input type="date" name="shifts[]" style="width:100%" /></div>' +
+            '<div class = "col-md-3"><input type="time" name="shifts[]" style="width:100%"/></div><div class = "col-md-3">' +
+            '<input type="time" name="shifts[]" style="width:100%"/></div><a href="#" class="remove_field">Remove</a></div></div>')
+        }
+    });
+
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
+</script>
