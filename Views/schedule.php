b3f2daee9caeffbf4ad3c99f2899e3240f697fcf
df1217 <dffrank1217@gmail.com>
2017-03-30 18:19:09 -0700

    Added header and footer to views... issues with session and navigatoin

--- Views/schedule.php
+++ Views/schedule.php
@@ -1,48 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <title>LCC CIT Lab Schedule</title>
- 
- <link rel="stylesheet" type="text/css"
-          href="./Styles/main.css">
-		  
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- 
-</head>
-
-
-<div class="title">
-  <div class="container text-center">
-    <h1>Lane Community College CIT Lab</h1>      
-  </div>
-</div>
-
-
-	<nav class="navbar navbar-default">
-	  <div class="container-fluid">
-		<div class="navbar-header">
-		  <a class="navbar-brand" href="#">CIT Lab</a>
-		</div>
-		<div class="collapse navbar-collapse" id="navbar">
-		  <ul class="nav navbar-nav">
-			<li><a href="?action=home">Home</a></li>
-			<li class="active"><a href="#">Schedule</a></li>
-			<li><a href="?action=ask">Questions</a></li>
-		  </ul>
-		  <ul class="nav navbar-nav navbar-right">
-			<li><a href="#"><span class="glyphicon glyphicon-envelope"></span></a></li>
-			<li><a href="?action=logout"><span ></span> Logout</a></li>
-		  </ul>
-		</div>
-	  </div>
-	</nav>
-
-<body>
+<?php include 'header.php'; ?>
 <div class="container-fluid" style="width:750px;">    
 	  <div class="row content">
 		
@@ -85,4 +41,4 @@
 		</div>
 	  </div>
 	</div>
-</body>
+<?php include 'footer.php'; ?>
