ef1db2382d1da4b2a6d066d4b6ad2cc3af08795d
df1217 <dffrank1217@gmail.com>
2017-04-19 20:58:29 -0700

    delete double header on edit profile remove edit email

--- Views/edit.php
+++ Views/edit.php
@@ -1,62 +1,52 @@
  <?php include 'header.php'; ?>
 <div class="container-fluid">
 	<?php $user = $_SESSION['user'];?>
-  	<div class="title">
-	  <div class="container text-center">
-		<h1>Lane Community College CIT Lab Edit Profile</h1>
-	  </div>
-	</div>
-	
+  	
 	<div class="row">
 		<div class="col-lg-12"><br /></div>
 	</div>
-	
+
 	<div class="row">
       <!-- left column -->
       <div class="col-md-3">
-	   <h2 class="text-center"><?php echo($user->getFirstName(). " " . $user->getLastName());?></h2>   
+	   <h2 class="text-center"><?php echo($user->getFirstName(). " " . $user->getLastName());?></h2>
         <div class="text-center">
           <img src="./Styles/smiley.png
 		  " class="avatar img-circle" alt="avatar">
           <h6>Upload a different photo...</h6>
           <input type="file" class="form-control" style="display: block;">
 		  </br>
-		  
+
         </div>
-      </div> 	  
+      </div>
       <!-- edit form column -->
       <div class="col-md-9 personal-info">
-	
-          
+
+
         <br />
 		<form class="form-horizontal" method="post" action=".?=edit_profile" id="edit_form">
 		<input type="hidden" name="action" value="edit_profile" />
-		
-		
-          <div class="form-group">
-		  <label class="col-md-3 control-label">Email:</label>
-			<div class="col-md-8">
-				<input class="form-control" id="email" name="email" type="text" value="<?php echo($user->getEmail());?>" required>
-			</div>
-          </div>
-		  
+
+
+
+
           <div class="form-group">
             <label class="col-md-3 control-label">New Password:</label>
             <div class="col-md-8">
               <input class="form-control" id="newPwd1" name="newPwd1" type="password" value="">
             </div>
           </div>
-		  
+
           <div class="form-group">
             <label class="col-md-3 control-label">New password:</label>
             <div class="col-md-8">
               <input class="form-control" id="newPwd2" name="newPwd2" type="password" value="">
             </div>
           </div>
-		  
+
           <div class="form-group">
             <label class="col-md-3 control-label"></label>
-            <div class="col-md-8 form-actions">	  
+            <div class="col-md-8 form-actions">
 				<button type="save" class="btn btn-primary" id="saveProfile" name="saveProfile">Save Changes</button>
               <span></span>
 				<a href="?action=home"><input id="canelBtn" class="btn btn-default" type="button" value="Cancel"></a>
@@ -66,7 +56,7 @@
 				<span class="success pull-right"  style="color:green; font-weight:bold"><?php echo $success ?></span>
 				<span class="success pull-right"  style="color:red; font-weight:bold"><?php echo $passError ?></span>
 			</div>
-		  
+
         </form>
       </div>
   </div>
