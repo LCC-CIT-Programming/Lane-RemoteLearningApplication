e71c4bbaa5796fe32b615cad1934843d3bd43343
df1217 <dffrank1217@gmail.com>
2017-07-25 14:31:04 -0700

    add student populate script

--- Mock_Data/pop_student.php
+++ Mock_Data/pop_student.php
@@ -1,30 +1,40 @@
 <?php
-$file = fopen("mcl_app_user_student.txt","r");
+include 'C:\wamp\www\CIT_Remote_Monitoring_App\App\Models\db.php';
+$lines = file("mcl_app_user_student.txt");
+$db = Database::getDb();
 
-while(! feof($file))
-  {
-  echo fgets($file). "<br />";
-  }
-
-fclose($file);
-
-$file = fopen("mcl_app_user_student.txt","r");
-
-while(! feof($file))
-  {
-	$line = fgets($file);
-	$trim_line = preg_replace('/\s+/', '', $line);
-	echo $trim_line . "<br />";
+foreach($lines as $line)
+{
+    $trim_line = preg_replace('/\s+/', "", $line);
 	$parts = explode('"', $trim_line);
-	
+	$output = array();
 	foreach ($parts as $part)
 	{
+			
 		if ( $part != "")
+		{
+			array_push($output, $part);			
+		}
 		
-		echo $part . "<br />";
 	}
-	
-  }
 
-fclose($file);
+	$lnum = $output[0];
+	$firstName = $output[2];
+	$lastName = $output[1];
+	$email = end($output);
+	
+	$query1 = 'INSERT INTO AppUser(FirstName, LastName, LNumber, EmailAddress)
+							 VALUES (:firstName, :lastName, :lnum, :email)';
+	$statement = $db->prepare($query1);
+	$statement->bindValue(':firstName', $firstName);
+    $statement->bindValue(':lastName', $lastName);
+    $statement->bindValue(':lnum', $lnum);
+    $statement->bindValue(':email', $email);
+	$statement->execute();
+	$statement->closeCursor();
+	echo $lnum . "<br />" . $lastName . "<br />" . $firstName . "<br />" . $email . "<br />";
+	
+	
+}
+
 ?>
