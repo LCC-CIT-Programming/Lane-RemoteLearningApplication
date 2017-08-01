33882f243b7fd71385eef970c4d9f71055bbb8fe
df1217 <dffrank1217@gmail.com>
2017-07-31 20:40:08 -0700

    Remove Duplicates

--- Mock_Data/pop_student.php
+++ Mock_Data/pop_student.php
@@ -1,7 +1,7 @@
 <?php
 
 //include 'C:\wamp\www\CIT_Remote_Monitoring_App\App\Models\db.php';
-include($_SERVER["DOCUMENT_ROOT"] . "CIT_Remote_Monitoring_App/App/Models/db.php");
+include($_SERVER["DOCUMENT_ROOT"] . "/CIT_Remote_Monitoring_App/App/Models/db.php");
 $lines = file("mcl_app_user_student.txt");
 $db = Database::getDb();
 foreach($lines as $line)
@@ -25,15 +25,30 @@ foreach($lines as $line)
 	$lastName = $output[1];
 	$email = end($output);
 	
-	$query1 = 'INSERT INTO AppUser(FirstName, LastName, LNumber, EmailAddress)
-							 VALUES (:firstName, :lastName, :lnum, :email)';
+	$query1 = 'SELECT LNumber FROM AppUser WHERE LNumber = :lnum';
 	$statement = $db->prepare($query1);
+	$statement->bindValue(':lnum', $lnum);
+	$statement->execute();
+	$rows = $statement->fetchAll();
+	$statement->closeCursor();
+	
+	
+	echo count($rows) . "<br/>";
+	
+	if (count($rows) == 0)
+	{
+	
+	$query2 = 'INSERT INTO AppUser(FirstName, LastName, LNumber, EmailAddress)
+							 VALUES (:firstName, :lastName, :lnum, :email)';
+	
+	$statement = $db->prepare($query2);
 	$statement->bindValue(':firstName', $firstName);
     $statement->bindValue(':lastName', $lastName);
     $statement->bindValue(':lnum', $lnum);
     $statement->bindValue(':email', $email);
 	$statement->execute();
 	$statement->closeCursor();
+	}
 	echo $lnum . "<br />" . $lastName . "<br />" . $firstName . "<br />" . $email . "<br />";
 	
 }
